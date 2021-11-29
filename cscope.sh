3304c61179105e4174fa5267a22655b00dc229bb
Eric Nystrom <eric.nystrom@yahoo.com>
2021-11-29 05:50:10 -0500

    Update location for cscope files.

--- cscope.sh
+++ cscope.sh
@@ -2,6 +2,5 @@
 
 find . -name "*.[ch]" >cscope.files
 find . -name "*.cpp" >>cscope.files
-find ../LatheCPP/src -name "*.cpp" >>cscope.files
-find ../LatheCPP/include -name "*.h" >>cscope.files
-
+find ../LatheCPP/lathe_src -name "*.cpp" >>cscope.files
+find ../LatheCPP/lathe_src -name "*.h" >>cscope.files
